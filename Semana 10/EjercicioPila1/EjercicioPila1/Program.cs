1445dd889ad55ea31d2c9c6e8eb13ef0a2d73e64
Efren <ejimenezdelgado@gmail.com>
2019-04-17 23:07:32 -0600

    clase 12

---
+++ Semana 10/EjercicioPila1/EjercicioPila1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPila1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("El mejor sistemita");
+            int cantidad = 0;
+            Console.WriteLine("Digite la cantidad de numero");
+            cantidad = Int32.Parse(Console.ReadLine());
+            int digitado = 0;
+            int suma = 0;
+
+            Stack miPila = new Stack();
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Digite el numero");
+                digitado = int.Parse(Console.ReadLine());
+                miPila.Push(digitado);
+            }
+            for (int i = 0; i < cantidad; i++)
+            {
+                suma = suma + int.Parse(miPila.Pop() + "");
+            }
+            Console.WriteLine("La suma " + suma);
+            Console.ReadLine();
+        }
+    }
+}
